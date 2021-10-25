761e816d35cd32f5c7f0e102ded64866678eb166
jepkess <biivincent1997@gmail.com>
2021-10-25 22:42:09 +0300

    add the movie.html on to the template

--- app/views.py
+++ app/views.py
@@ -7,4 +7,13 @@ def index():
     '''
     view root page function that returns the index page and its data.
     '''
-    return render_template('index.html') # response 
+    message= "hello world!" # message represent the one in the template
+    return render_template('index.html', message) # response 
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
