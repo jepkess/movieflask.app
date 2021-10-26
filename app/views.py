5e1b3107ce457199836a46cabaf8e92c22e7020e
jepkess <biivincent1997@gmail.com>
2021-10-26 23:33:19 +0300

    create the macro.html file on the template folder

--- app/views.py
+++ app/views.py
@@ -3,7 +3,6 @@ from app import app
 from .request import get_movies
 
 #views
-
 @app.route('/')
 def index():
 
@@ -13,7 +12,9 @@ def index():
 
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)    
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+      
 
