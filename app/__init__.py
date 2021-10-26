60d65f9f66d0158063c8e3c5245f4518032abb5a
jepkess <biivincent1997@gmail.com>
2021-10-27 00:14:47 +0300

    create a navbar.html on the template folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 
 
@@ -11,5 +12,8 @@ app= Flask(__name__,instance_relative_config = True)  # app instances we pass in
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
