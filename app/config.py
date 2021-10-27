fd001a105f409a7548a5476b29d1472935337428
jepkess <biivincent1997@gmail.com>
2021-10-27 20:31:49 +0300

    creating a the form.py file on the app folder

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     '''
     MOVIE_API_KEY='1ae99d3bc681cc606e31ddc9de6be3bb'
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    SECRET_KEY = '<Flask WTF Secret Key>'
 
 
 
