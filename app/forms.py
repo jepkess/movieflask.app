7027644d0def21da2c0bd36748c8de8f0bf58269
jepkess <biivincent1997@gmail.com>
2021-10-27 21:44:08 +0300

    create a new.review.html file that display the form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from flask_wtf import FlaskForm #help us to create  the form Class
+from wtforms import StringField,TextAreaField,SubmitField 
+from wtforms.validators import Required #validator that preventthe users from submitting form without filling it.
 
 class ReviewForm(FlaskForm):
     title = StringField('Review title',validators=[Required()])
