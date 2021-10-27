1090aa63a97f540dd5c34fd9d0378b5a18c04564
jepkess <biivincent1997@gmail.com>
2021-10-28 00:30:43 +0300

    displaying the movie reviews

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ class Config:
     '''
     General configuration parent class
     '''
-    MOVIE_API_KEY='1ae99d3bc681cc606e31ddc9de6be3bb'
+    
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    SECRET_KEY = '<Flask WTF Secret Key>'
+    
 
 
 
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
-from flask_wtf import FlaskForm #help us to create  the form Class
-from wtforms import StringField,TextAreaField,SubmitField 
-from wtforms.validators import Required #validator that preventthe users from submitting form without filling it.
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
 
 class ReviewForm(FlaskForm):
+
     title = StringField('Review title',validators=[Required()])
     review = TextAreaField('Movie review', validators=[Required()])
     submit = SubmitField('Submit')
