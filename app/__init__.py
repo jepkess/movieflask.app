ff79de46f501d847b6049df06b97fc56879f0550
jepkess <biivincent1997@gmail.com>
2021-10-29 12:39:28 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
 
-    app = Flask(__name__)
+    app = Flask(__name__,template_folder='templates')
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
