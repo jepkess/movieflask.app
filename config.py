c948feec2e4cba6ded73424c19e502e4212a8619
jepkess <biivincent1997@gmail.com>
2021-11-04 15:43:54 +0300

    config sqlalchemy database url

--- config.py
+++ config.py
@@ -7,6 +7,15 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations connecting to the email.
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 
 
@@ -28,10 +37,14 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/watchlist_test'    
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URL='postgresql+psycopg2://moringa:1234@localhost/watchlist'
     DEBUG = True
  # cinfig_options that help us access different configuration option classes.
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig,
 }
