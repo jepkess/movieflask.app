be35febbec1e59b0c142aaeb61536af1b62a4507
jepkess <biivincent1997@gmail.com>
2021-11-04 21:55:23 +0300

    create class user , and columns and migrate data to the database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id): # call back function that retrieves a user when a unique identifier is passed.
+    return User.query.get(int(user_id))
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -42,3 +51,93 @@ class Review:
                 response.append(review)
 
         return response
+class User(UserMixin,db.Model): #db.model connect class to the database and allow communication.
+    __tablename__ = 'users'# tablename gives allows us to give the tables proper names. 
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    reviews=db.relationship('Review',backref='user',lazy="dynamic")
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255)) #column for bio 
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)    
+    
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+
+    def __repr__(self):
+        return f'User {self.username}'     
+
+                
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+class Review(db.Model): #db.model creates the connection to our database.
+    __tablename__ = 'reviews'
+
+    
+    id = db.Column(db.Integer,primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted=db.Column(db.DateTime,default=datetime.utcnow) # gets the current time  and saves it to our database.    
+    user_id=db.Column(db.Integer,db.ForeignKey("users.id")) # we use the foreign key column to store the id of the users they wrote the review.
+
+    def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_reviews(cls,id):
+        reviews = Review.query.filter_by(movie_id=id).all()
+        return reviews    
+# class User(db.Model):
+#     __tablename__ = 'users'
+
+#     id = db.Column(db.Integer,primary_key = True)
+#     username = db.Column(db.String(255))
+    
+
+
+#     def __repr__(self):
+#         return f'User {self.username}'
+
+
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+
+#     id = db.Column(db.Integer,primary_key = True)
+#     name = db.Column(db.String(255))
+   
+
+
+    # def __repr__(self):
+    #     return f'User {self.name}'   
+
+
+
+
+
+
+
+
+
+        
+                             
